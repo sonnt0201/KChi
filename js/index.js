fb55a9c20b0829addf85084629595c5ce01d084b
MTD-SONNT290\Dell <nguyenthaison0201@gmail.com>
2022-10-05 18:26:09 +0700

    add features

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var string = [
+  "Chuc idol som co nguoi thuong",
+  "baby em nhu bong hoa ~.~",
+  "nhung nguoi hai dau phai taaa",
+  "em voi mang bao cau ca <3",
+  "troi ve noi xa xa xa ==.==",
+  "em nhu bong hoa :<<",
+  "nhung nguoi hai dau phai ta T_T",
+  "em voi mang bao cau ca 🎶",
+  "troi ve noi xa xa ...",
+];
+var i = 0;
+function change(isForwards) {
+  if (isForwards && i < string.length - 1) {
+    i++;
+  } else if (i > 0) {
+    i--;
+  }
+  document.getElementById("content").innerHTML = string[i];
+}
